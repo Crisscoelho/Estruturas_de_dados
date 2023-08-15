031ee5e1ed8c5c9419fcf8f58e32a50bf30559d3
Cristina Coelho <crissmcoelho@yahoo.com.br>
2023-08-15 16:48:14 -0300

    Primeiro commit

---
+++ variaveisEmemoria/variaveisEmemoria.c
@@ -0,0 +1,16 @@
+#include <stdio.h>     //biblioteca c
+
+int main(){
+
+    int a = 10;
+    int b, c;
+    b = 20;
+    c = a + b;
+
+    printf("c = %d\n", c);
+    printf("&c = %p", c);
+
+    return 0;
+
+
+}
